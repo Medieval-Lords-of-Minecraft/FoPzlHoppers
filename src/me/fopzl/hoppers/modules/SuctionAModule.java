9bca13e5c74626e4eacdfec428de0a58a123b7d7
FoPzl <officialsuperalex11@gmail.com>
2023-02-03 20:40:27 -0500

    Tick rate delta fix

--- src/me/fopzl/hoppers/modules/SuctionAModule.java
+++ src/me/fopzl/hoppers/modules/SuctionAModule.java
@@ -12,57 +12,60 @@ public class SuctionAModule extends HopperModule {
 	private static final int maxTickPeriod = 200;
 	private static final int tickPeriodDelta = 10; // period increase when no items picked up, or decrease when any are
 	private static final int suckRange = 3;
-
-	private static final int suckRangeSquared = suckRange * suckRange;
 	
+	private static final int suckRangeSquared = suckRange * suckRange;
+
 	private int ticks;
 	private int tickPeriod;
-	
+
 	public SuctionAModule(Hopper hopper) {
 		super(hopper);
-		
-		ticks = 0;
+
 		tickPeriod = minTickPeriod;
+		ticks = tickPeriod;
 	}
-	
+
 	@Override
 	public void tick() {
-		if (++ticks % tickPeriod != 0)
+		if (--ticks > 0)
 			return;
-		
-		Entity[] nearbyEntities = hopper.getLocation().getChunk().getEntities();
-		if (nearbyEntities.length == 0)
-			incTickPeriod();
 
-		for (Entity e : nearbyEntities) {
+		boolean pickedUp = false;
+		
+		for (Entity e : hopper.getLocation().getChunk().getEntities()) {
 			// instanceof is more performant than Entity.getType()
 			if (e instanceof Item item && item.getPickupDelay() == 0 && e.getLocation().distanceSquared(hopper.getLocation()) < suckRangeSquared) {
 				int startAmt = item.getItemStack().getAmount();
-
+				
 				ItemStack leftovers = hopper.getInventory().addItem(item.getItemStack()).get(0);
 				if (leftovers == null) {
 					item.remove();
-					decTickPeriod();
+					pickedUp = true;
 				} else {
 					item.setItemStack(leftovers);
-
+					
 					int endAmt = leftovers.getAmount();
 					if (endAmt != startAmt)
-						decTickPeriod();
-					else
-						incTickPeriod();
+						pickedUp = true;
 				}
 			}
 		}
-	}
+		
+		if (pickedUp)
+			decTickPeriod();
+		else
+			incTickPeriod();
 
+		ticks = tickPeriod;
+	}
+	
 	// called when no items get picked up
 	private void incTickPeriod() {
 		tickPeriod += tickPeriodDelta;
 		if (tickPeriod > maxTickPeriod)
 			tickPeriod = maxTickPeriod;
 	}
-
+	
 	// called when any items get picked up
 	private void decTickPeriod() {
 		tickPeriod -= tickPeriodDelta;
