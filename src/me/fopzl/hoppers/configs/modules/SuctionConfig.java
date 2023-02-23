45e3991ae7bf83fb6f2409aa366e1ce9841e08b4
FoPzl <officialsuperalex11@gmail.com>
2023-02-23 09:08:13 -0500

    Prep work for player-editable config settings

--- src/me/fopzl/hoppers/configs/modules/SuctionConfig.java
+++ src/me/fopzl/hoppers/configs/modules/SuctionConfig.java
@@ -7,19 +7,19 @@ public class SuctionConfig extends ModuleConfig {
 	private int maxTickPeriod;
 	private int tickPeriodDelta; // period increase when no items picked up, or decrease when any are
 	private int suckRange;
-	
-	private int suckRangeSquared;
 
+	private int suckRangeSquared;
+	
 	@Override
 	public void loadConfig(ConfigurationSection config) {
 		minTickPeriod = config.getInt("minTickPeriod", minTickPeriod);
 		maxTickPeriod = config.getInt("maxTickPeriod", maxTickPeriod);
 		tickPeriodDelta = config.getInt("tickPeriodDelta", tickPeriodDelta);
 		suckRange = config.getInt("suckRange", suckRange);
-
+		
 		suckRangeSquared = suckRange * suckRange;
 	}
-	
+
 	@Override
 	public SuctionConfig clone() {
 		SuctionConfig cfg = new SuctionConfig();
@@ -27,28 +27,33 @@ public class SuctionConfig extends ModuleConfig {
 		cfg.maxTickPeriod = this.maxTickPeriod;
 		cfg.tickPeriodDelta = this.tickPeriodDelta;
 		cfg.suckRange = this.suckRange;
-
-		cfg.suckRangeSquared = this.suckRangeSquared;
 		
+		cfg.suckRangeSquared = this.suckRangeSquared;
+
 		return cfg;
 	}
-	
+
 	public int getMinTickPeriod() {
 		return minTickPeriod;
 	}
-	
+
 	public int getMaxTickPeriod() {
 		return maxTickPeriod;
 	}
-	
+
 	public int getTickPeriodDelta() {
 		return tickPeriodDelta;
 	}
-	
+
+	public void setSuckRange(int range) {
+		suckRange = range;
+		suckRangeSquared = range * range;
+	}
+
 	public int getSuckRange() {
 		return suckRange;
 	}
-
+	
 	public int getSuckRangeSquared() {
 		return suckRangeSquared;
 	}
